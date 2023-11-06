ffb138a0410ceade9c852138468f8e1a28eeeab4
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-11-06 10:40:26 +0330

    exposing the consumers

--- src/dtos/deletedBill.dto.ts
+++ src/dtos/deletedBill.dto.ts
@@ -16,6 +16,10 @@ export class DeletedBillDto {
 
   @Expose()
   @ApiProperty()
+  consumers: string[];
+
+  @Expose()
+  @ApiProperty()
   description: string;
 
   @Expose()
--- src/dtos/updateBill.dto.ts
+++ src/dtos/updateBill.dto.ts
@@ -1,5 +1,15 @@
-import { IsString, IsNumber, IsNumberString, Length } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  IsNumberString,
+  Length,
+  IsArray,
+  ArrayMinSize,
+  ArrayMaxSize,
+  Validate,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsConsumers } from 'src/validators';
 
 export class UpdateBillDto {
   @IsNumberString()
@@ -15,6 +25,14 @@ export class UpdateBillDto {
   @Length(3, 100)
   @ApiProperty()
   receiver: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  @ArrayMaxSize(20)
+  @Validate(IsConsumers, { message: 'Each consumer has to have less than 100 length.' })
+  @ApiProperty()
+  consumers: string[];
 
   @IsString()
   @Length(3, 500)
