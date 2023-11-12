81eb47ba8ed32621d413a9c8745dc2ff3b074462
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-11-12 20:10:42 +0330

    removing the regix for consumer filters

--- src/dtos/consumerListFilters.dto.ts
+++ src/dtos/consumerListFilters.dto.ts
@@ -1,10 +1,9 @@
-import { IsString, MaxLength, Matches } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ConsumerListFiltersDto {
   @IsString()
   @MaxLength(100)
-  @Matches(/^[a-zA-Z_]+( [a-zA-Z_]+)*$/, { message: 'Invalid consumer name.' })
   @ApiProperty()
   q: string;
 }
