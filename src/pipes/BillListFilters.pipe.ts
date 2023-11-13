e0d1b950f0189d4e2acf02645db5a063fd75c161
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-11-13 22:25:56 +0330

    dummy change

--- src/pipes/BillListFilters.pipe.ts
+++ src/pipes/BillListFilters.pipe.ts
@@ -2,6 +2,8 @@ import { ArgumentMetadata } from '@nestjs/common';
 import { Injectable, PipeTransform } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 
+console.log('');
+
 @Injectable()
 export class ParseBillListFiltersPipe implements PipeTransform {
   async transform(value: any, { metatype }: ArgumentMetadata) {
