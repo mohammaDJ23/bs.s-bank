34c107c64bec7b20cd8e9b2f3c44b3b5ecc58994
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-01-25 11:09:29 +0330

    removing ttl

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -35,14 +35,12 @@ import {
       useFactory: async () => ({
         isGlobal: true,
         store: await redisStore({
-          ttl: +process.env.REDIS_TTL,
           url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
           password: process.env.REDIS_PASSWORD,
           username: 'default',
         }),
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
-        ttl: +process.env.REDIS_TTL,
       }),
     }),
     ScheduleModule.forRoot(),
