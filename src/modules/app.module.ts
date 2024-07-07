6a560352e8fbccb9b37f07a7440ab0466795ab4b
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-07-07 09:00:42 +0330

    revising the  synchronize

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -82,7 +82,7 @@ import {
         database: process.env.DATABASE_NAME,
         namingStrategy: new CustomNamingStrategy(),
         entities: [Bill, User, Consumer, Receiver, Location],
-        synchronize: true,
+        synchronize: process.env.NODE_ENV === 'development',
       }),
     }),
     TypeOrmModule.forFeature([Bill, User, Consumer, Receiver, Location]),
