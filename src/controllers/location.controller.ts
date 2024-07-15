fd68b9daa42116b28a934c31210a411a08e2e5af
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-07-15 11:16:40 +0330

    adding a default values for pipes

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -7,6 +7,7 @@ import {
   ParseIntPipe,
   Query,
   UseInterceptors,
+  DefaultValuePipe,
 } from '@nestjs/common';
 import { ApiTags, ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import { CurrentUser } from 'src/decorators';
@@ -34,9 +35,10 @@ export class LocationController {
   @ApiResponse({ status: HttpStatus.UNAUTHORIZED, type: ErrorDto })
   @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, type: ErrorDto })
   findAll(
-    @Query('page', ParseIntPipe) page: number,
-    @Query('take', ParseIntPipe) take: number,
-    @Query('filters', ParseLocationListFiltersPipe) filters: LocationListFiltersDto,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('take', new DefaultValuePipe(10), ParseIntPipe) take: number,
+    @Query('filters', new DefaultValuePipe(new LocationListFiltersDto()), ParseLocationListFiltersPipe)
+    filters: LocationListFiltersDto,
     @CurrentUser() user: User,
   ): Promise<[Location[], number]> {
     return this.locationService.findAll(page, take, filters, user);
