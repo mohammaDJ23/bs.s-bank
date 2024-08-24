dcdb91a632adbd48b9cf4a62e775f4c3997a75da
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-08-24 21:24:02 +0330

    adding the defalut values

--- src/controllers/bill.controller.ts
+++ src/controllers/bill.controller.ts
@@ -166,9 +166,10 @@ export class BillController {
   @ApiResponse({ status: HttpStatus.UNAUTHORIZED, type: ErrorDto })
   @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, type: ErrorDto })
   findAll(
-    @Query('page', ParseIntPipe) page: number,
-    @Query('take', ParseIntPipe) take: number,
-    @Query('filters', ParseAllBillListFiltersPipe) filters: AllBillListFiltersDto,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('take', new DefaultValuePipe(10), ParseIntPipe) take: number,
+    @Query('filters', new DefaultValuePipe(new AllBillListFiltersDto()), ParseAllBillListFiltersPipe)
+    filters: AllBillListFiltersDto,
   ): Promise<[Bill[], number]> {
     return this.billService.findAll(page, take, filters);
   }
